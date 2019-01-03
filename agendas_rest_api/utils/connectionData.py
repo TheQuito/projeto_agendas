9be0706d9b3c69f64cd6cfd2aff9ca62d482d18b
JonesQuito <jonesdhy@hotmail.com>
2019-01-03 11:38:49 -0200

    classe de metadados da conexão

---
+++ agendas_rest_api/utils/connectionData.py
@@ -0,0 +1,15 @@
+import cx_Oracle
+
+# ESTA CLASSE DEVE SER EDITADA COM OS DADOS DE CONEXÃO
+class ConnectionData:
+	def __init__(self, arg):
+		self.user = ''
+		self.password = ''
+		self.host = ''
+		self.db = ''
+
+
+	def __del__(self):
+		del self
+
+		
