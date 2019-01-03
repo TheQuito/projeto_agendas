ac4e6c79767c242798f79b6157db063822457138
JonesQuito <jonesdhy@hotmail.com>
2019-01-03 17:14:18 -0200

    classe de conexao com o banco de dados

--- agendas_rest_api/utils/conexao.py
+++ agendas_rest_api/utils/conexao.py
@@ -1,25 +1,41 @@
 import cx_Oracle
-from connectionData import ConnectionData 
+from agendas_rest_api.utils.connectionData import ConnectionData 
 
 class Conexao:
 
 	global conn
 
 	# Método construtor
-	def __init__(self, user, password, host, db):
+	def __init__(self):
 		connectionData = ConnectionData()
 		self.user = connectionData.user
 		self.password = connectionData.password
 		self.host = connectionData.host
 		self.db = connectionData.db
+		self.conn = None
 
+
+	# Método responsável por retornar a conexão para o usuário
+	def obterConexao(self):
 		try:
-			
+			conn = cx_Oracle.connect(self.user+'/'+self.password+'@'+self.host+'/'+self.db)
+			return conn
+		except:
+			print('Erro ao conectar no banco')
 
-		super(Conexao, self).__init__()
-		self.arg = arg
 
 
 	# Método destrutor
 	def __del__(self):
 		del self
+
+
+   
+
+        
+        
+
+   
+         
+        
+    
