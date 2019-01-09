f5f9eb0868b42f294b6a768dbb32e18225905742
JonesQuito <jonesdhy@hotmail.com>
2019-01-09 17:22:01 -0200

    Dados de conexao

---
+++ agendas_rest_api/utils/connectionData.py
@@ -0,0 +1,16 @@
+
+# ESTA CLASSE DEVE SER EDITADA COM OS DADOS DE CONEXÃO
+class ConnectionData:
+	def __init__(self):
+		self.user = ''
+		self.password = ''
+		self.host = ''
+		self.db = ''
+
+
+
+	def __del__(self):
+		del self
+
+
+		
